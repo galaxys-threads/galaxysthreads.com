a8c1eb3b3418fa3505e19d5156f3770717538562
Aaron Ellington <aaron@ellington.io>
2022-06-09 21:33:12 +0000

    wip

---
+++ themes/main/src/data/canon.ts
@@ -0,0 +1,66 @@
+export enum CanonType {
+    skywalkerSagaFilm = "Skywalker Saga Film",
+    film = "Film",
+    game = "Video Game",
+    show = "TV Show",
+    book = "Novel",
+}
+
+export enum CanonEra {
+    highRepublic = "The High Republic",
+    republic = "The Republic",
+    empire = "The Empire",
+    newRepublic = "The New Republic",
+    firstOrder = "The First Order",
+}
+
+export interface CanonEntry {
+    name: string,
+    type: CanonType,
+    disneyPlus?: string,
+    wookieepedia: string,
+    released: Date,
+    timeline: number,
+}
+
+export function formatTimeline(timeline: number): string {
+    if (timeline < 0) {
+        return `${timeline * -1} BBY`
+    }
+
+    if (timeline > 0) {
+        return `${timeline} ABY`
+    }
+
+
+    return "0 BBY / 0 ABY"
+}
+
+const Canon: CanonEntry[] = [
+    {
+        name: "A New Hope",
+        type: CanonType.skywalkerSagaFilm,
+        timeline: 0,
+        released: new Date("1977-05-25"),
+        wookieepedia: "https://starwars.fandom.com/wiki/Star_Wars:_Episode_IV_A_New_Hope",
+        disneyPlus: "https://www.disneyplus.com/movies/star-wars-a-new-hope-episode-iv/12fVeZxD2fWJ",
+    },
+    {
+        name: "The Empire Strikes Back",
+        type: CanonType.skywalkerSagaFilm,
+        timeline: 3,
+        released: new Date("1980-05-12"),
+        wookieepedia: "https://starwars.fandom.com/wiki/Star_Wars:_Episode_V_The_Empire_Strikes_Back",
+        disneyPlus: "https://www.disneyplus.com/movies/star-wars-the-empire-strikes-back-episode-v/iqtDTZAewwYl",
+    },
+    {
+        name: "Return of the Jedi",
+        type: CanonType.skywalkerSagaFilm,
+        timeline: 4,
+        released: new Date("1983-05-25"),
+        wookieepedia: "https://starwars.fandom.com/wiki/Star_Wars:_Episode_VI_Return_of_the_Jedi",
+        disneyPlus: "https://www.disneyplus.com/movies/star-wars-return-of-the-jedi-episode-vi/6QGKo5mjDBS8",
+    },
+];
+
+export default Canon
--- themes/main/src/pages/CanonGuide.tsx
+++ themes/main/src/pages/CanonGuide.tsx
@@ -1,3 +1,54 @@
-export default  function CanonGuide() {
-    return <h1>Hello world!</h1>;
-  }
+import DataTable, { TableColumn } from 'react-data-table-component';
+
+import Canon, {CanonEntry, formatTimeline} from "../data/canon"
+const columns: TableColumn<CanonEntry>[] = [
+    {
+        name: 'Name',
+        cell: row => {
+            let disneyPlus = <></>
+            if (row.disneyPlus) {
+                disneyPlus = <a href={row.disneyPlus} target="_blank" title="Watch on Disney+" className='disneyPlusLink'>
+                    <i className="fa-solid fa-circle-play"></i>
+                </a>
+            }
+
+            return <div>
+                <a href={row.wookieepedia} target="_blank" title="View on Wookieepedia">
+                    {row.name}
+                </a>
+                {disneyPlus}
+            </div>
+        },
+        sortable: true,
+    },
+    {
+        name: 'Type',
+        selector: row => row.type,
+        sortable: true,
+    },
+
+    {
+        name: 'Timeline',
+        cell: (row) => {
+            return formatTimeline(row.timeline)
+        },
+        sortable: true,
+        sortFunction: (rowA, rowB) => {
+            return rowA.timeline > rowB.timeline ? 1 : -1
+        },
+    },
+    {
+        name: 'Released',
+        selector: row => row.released.getFullYear(),
+        sortable: true,
+    },
+];
+
+export default function CanonGuide() {
+    return <div>
+        <DataTable
+            columns={columns}
+            data={Canon}
+        />
+    </div>
+}
