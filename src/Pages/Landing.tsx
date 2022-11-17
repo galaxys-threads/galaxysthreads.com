43a749a446e02080a34b9b432bd0cf579d9b7240
Aaron Ellington <aaron@ellington.io>
2022-11-17 23:07:43 +0000

    update 404 page

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Landing() {
-	return <h1>Landing Page</h1>
+	return (
+		<div className="container">
+			<p>Landing Page</p>
+		</div>
+	)
 }
--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
-	return <p>This isn't the page you are looking for.</p>
+	return (
+		<div className="container">
+			<p>
+				This isn't the page you are looking for...{' '}
+				<Link to="/">Move along</Link>.
+			</p>
+		</div>
+	)
 }
